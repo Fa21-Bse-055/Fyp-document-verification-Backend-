a3504b0220298ed3d79c79a5a0df5a408ad1ab62
user.name  : "ahmaranwar24@gmail.com <ahmaranwar24@gmail.com>
2024-09-11 11:09:49 +0500

    Registration Module In-Progress

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.MONGO_URI);
+  await mongoose.connect(process.env.MONGO_URI,{
+    serverSelectionTimeoutMS: 30000 // 30 seconds
+  });
   console.log("MongoDB Connected...");
 };
 
